40181be24b19acdaf9d39882a0aaee4e137b142d
Rosie-Brigham <rosiebrigham@gmail.com>
2020-09-03 14:45:21 +0100

    typo

--- export-model.sh
+++ export-model.sh
@@ -8,7 +8,7 @@ WORK_DIR="${CURRENT_DIR}/deeplab"
 
 python deeplab/export_model.py \
     --model_variant="xception_65" \
-    --checkpoint_path="${WORK_DIR}/datasets/PQR/exp/train_on_trainval_set/train/model.ckpt-200index" \
+    --checkpoint_path="${WORK_DIR}/datasets/PQR/exp/train_on_trainval_set/train/model.ckpt-200.index" \
     --export_path="${WORK_DIR}/datasets/PQR/exp/train_on_trainval_set/trained_model" \
     --num_classes=2 \
     --crop_size=513 \
